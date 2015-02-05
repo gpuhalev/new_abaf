7fdcd64128217484ca7d1c87cdb7a9e7afba9595
gpuhalev <gesho.puh@gmail.com>
2015-02-05 23:50:11 +0200

    +structure

--- structure.php
+++ structure.php
@@ -39,12 +39,66 @@
                     <p class="content_title">ABAF Commissions</p>
                     
                     <div class="grayOutlineRoundedCV">
-                    	<p class="green_13">Click here to view</p>
+                    	<a href="#"><p class="green_13">Click here to view</p></a>
                     </div><div class="cleaner_h30"></div>
                     
                     <p class="content_title">ABAF Executive Committee</p>
                		<div class="grayOutlineRoundedCV">
                     	<img class="CV" src="images/structure/cv-karamarinov.jpg" name="karamarinov">
+                    	<p class="green_13" name="karamarinov">PRESIDENT: DOBROMIR KARAMARINOV / BUL<br>(PRESIDENT BULGARIAN ATHLETIC FEDERATION)</p>
+                        <div class="hiddenCV" name="karamarinov">
+                        	<h4> Curriculum Vitae:</h4>
+                            <ul>
+                            <li>Name: 		KARAMARINOV </li>
+                            <li>First name: 		DOBROMIR</li>
+                            <li>Date of birth:		26 March 1958</li>
+                            <li>Nationality:		Bulgarian </li>
+                            <li>Profession:		Track and Field coach</li>
+                            <li>Spoken Languages:	Bulgarian, English, Russian</li>
+                            </ul>
+
+                            <h4>Present Occupation:</h4>
+                            <ul>
+                            <li>since 2004 - President of Bulgarian Athletic Federation</li>
+                            </ul>
+
+                            <h4>Occupied positions:</h4>
+                            <ul>
+                            <li>since Oct. 2010	- President of the Association of the Balkan Athletics Federations (ABAF)</li>
+                            <li>since Aug. 2007	- Member of the IAAF Women's Committee</li>
+                            <li>since Apr. 2007	- European Athletics Council member</li>
+                            <li>2004 – 2007	- Member of the EAA Reflection Commission</li>
+                            <li>1995-2003	- Member of the EAA Competition Committee</li>
+                            <li>since 1994 ­	European Athletics Representative and Jury of Appeal at numerous International Meetings </li>
+                            <li>Meeting Director of multiple International Meetings</li>
+                            <li>Team Leader of the National Teams at all European and World Championships</li>
+                            <li>Delegate at all European Athletics  and IAAF Congresses</li>
+                            <li>Lecturer at the National Sports Academy</li>
+                            </ul>
+   
+                            <h4>Working Experience:</h4>
+                            <ul>
+                            <li>1994-2004	- General Secretary of Bulgarian Athletic Federation</li>
+                            <li>1993-1994	- Responsible for Olympic preparation in the Federation</li>
+                            <li>1991-1994	- Head Coach in Athletics</li>
+                            <li>1985-1990 - President of Yantra Gabrovo Sports Club</li>
+                            </ul>
+	
+                            <h4>Active Sport Career:</h4>
+                            <ul>
+                            <li>1972-1982	- 400mH Balkan and National Champion many times</li>
+                            </ul>
+                            
+                            <h4>Educational Background:</h4>
+                            <ul>
+                            <li>1978-1982	- National Sports Academy</li>
+                            <li>1972-1976	- National School of Mathematics</li>
+                            </ul>
+                        </div>
+                    </div>
+                    
+                    <div class="grayOutlineRoundedCV">
+                    	<img class="CV" src="images/structure/cv-karamarinov.jpg" name="fatih">
                     	<p class="green_13" name="karamarinov">PRESIDENT: DOBROMIR KARAMARINOV / BUL<br>(PRESIDENT BULGARIAN ATHLETIC FEDERATION)</p>
                         <div class="hiddenCV" name="karamarinov">
                         	<h4> Curriculum Vitae:</h4>
